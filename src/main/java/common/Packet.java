3b733671a66adc318adc9fba6338fc4a11edec93
Yandé Guissé <yandeiguiss2@gmail.com>
2026-03-03 01:44:35 +0000

    communication via socket

--- src/main/java/common/Packet.java
+++ src/main/java/common/Packet.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public class Packet implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     private String type;         // LOGIN, SEND_MSG, etc.
